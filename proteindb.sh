93243db1ee8b5ac7ed1b808ad225316b9ade9532
Jasmine Dumas <jasmine.dumas@gmail.com>
2015-09-30 17:16:43 -0400

    Create proteindb.sh

---
+++ proteindb.sh
@@ -0,0 +1,12 @@
+###########################################
+# Protein DB for MAKER-P
+# Jasmine Dumas
+# September 29 2015
+###########################################
+
+
+exonerate --model protein2genome /home/jasdumas/archive/sequence.fasta /archive/PineRefSeq/douglasfir/Psme_v1.0.fasta
+
+exonerate --model protein2genome /home/jasdumas/archive/eucalyptus.fasta.txt /archive/PineRefSeq/douglasfir/Psme_v1.0.fasta
+
+exonerate --model protein2genome /home/jasdumas/archive/Populustrichocarpa.fasta /archive/PineRefSeq/douglasfir/Psme_v1.0.fasta
