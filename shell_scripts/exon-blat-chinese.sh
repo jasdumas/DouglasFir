75a639859e0cb97234940f2b81cb3353102ef272
Jasmine Dumas <jasmine.dumas@gmail.com>
2015-11-13 12:41:48 -0500

    Update exon-blat-chinese.sh

--- shell_scripts/exon-blat-chinese.sh
+++ shell_scripts/exon-blat-chinese.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 
-#############################################################
+########### WALNUT Genome ###################################
 ##### TEMPLATE SGE SCRIPT - BLAST EXAMPLE ###################
 ##### /common/template_smp.sh ###############################
 #############################################################
-
-# Name the directory (assumed to be a direct subdir of $HOME) from which the file
-
-# listed in DATA_FILE is located and into which the output files will be deposited
-
+ 
 # Specify name to be used to identify this run
 
 #$ -N exon-blat-chinese
+                                                                                                                                                 # This sets the task range in the array from 1 to 4 with a step size of 1                                                                                              
+#$ -t 1-983:1     
+                                                                                                                                                       
+# This sets the maximum number of concurrent tasks to 10, so that no more than 2 jobs will be run at once                                                               
+#$ -tc 6
 
-# Specify High Prioity
+#$ -q highmem.q
 
-#$ -q highpri.q
+#$ -S /bin/bash 
+          
+#$ -pe smp 1
 
-# Specify that bash shell should be used to process this script
-
-#$ -S /bin/bash
-
-# Specify the output file
-
-#$ -o /archive/Walnut_genomes/exon-blat-chinese_$JOB_ID.out
+# Specify the output file                                                                         
+#$ -o exon-blat-chinese_$JOB_ID.out
 
 # Specify the error file
+#$ -e exon-blat-chinese_$JOB_ID.err
 
-#$ -e /archive/Walnut_genomes/exon-blat-chinese_$JOB_ID.err
+#$ -cwd                   
+#start=$(date + %s.%N)
 
-#$ -cwd
 # Run the program
-cd /archive/Walnut_genomes/exon-blat-chinese
+echo ChineseWalnut.500.scaffolds.fasta$SGE_TASK_ID.fa
 
-# exonerate format  exonerate query.fasta target.fasta
+# change working directory
+cd /archive/Walnut_genomes/
+
+# exonerate format exonerate --model protein2genome query.fasta target.fasta
+exonerate --model protein2genome --query ../spcombo.tfa --target /archive/Walnut_genomes/ChineseWalnut.500.scaffolds.fasta$SGE_TASK_ID.fa -n 1 --percent 70 --score 100 --minintron 20 --maxintron 800000 --showalignment no --showtargetgff yes --geneseed 250 --forcegtag --hspfilter 100 --showvulgar yes > ChineseWalnut.500.scaffolds.fasta$SGE_TASK_ID.gff
+
+#end=$(date +%s.%N)
+#runtime=$(python -c "print(${end}-${start})")
+
+#echo "Runtime for ChineseWalnut.500.scaffolds.fasta$SGE_TASK_ID.fa was $runtime"
