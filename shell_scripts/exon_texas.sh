6b124b5894929404db701f46f35d9bb8c919d563
Jasmine Dumas <jasdumas@users.noreply.github.com>
2015-12-11 13:55:10 -0500

    Create exon_texas.sh

---
+++ shell_scripts/exon_texas.sh
@@ -0,0 +1,38 @@
+# script that pulls just the scaffolds that received hits into a separate multi-FASTA file 
+# (genome scaffolds) for your next exonerate run
+# from sumaira on 11/13
+############# exon_texas.sh ###################
+#!/bin/bash                                                                                        
+#$ -N exon-tx 
+
+# This sets the task range in the array from 1 to 20 with a step size of 1                                                                                              
+#$ -t 1-20:1   
+
+# This sets the maximum number of concurrent tasks to 3                                                              
+#$ -tc 3
+
+# Specify that bash shell should be used to process this script
+#$ -S /bin/bash  
+
+# multiple cores per process (say 4 each)
+#$ -pe smp 4
+
+# Specify the output file                                                                         
+#$ -o exon-tx_$JOB_ID.out
+
+# Specify the error file
+#$ -e exon-tx_$JOB_ID.err
+
+#$ -cwd                   
+#start=$(date + %s.%N)
+# Run the program
+echo TexasWalnut.500.scaffolds.fasta$SGE_TASK_ID.fa
+
+cd /archive/Walnut_genomes/texas
+
+exonerate --model protein2genome --query /archive/Walnut_genomes/spcombo.tfa --target /archive/Walnut_genomes/texas/TexasWalnut.500.scaffolds.fasta$SGE_TASK_ID.fa -n 1 --percent 70 --score 100 --minintron 20 --maxintron 800000 --showalignment no --showtargetgff yes --geneseed 250 --forcegtag --hspfilter 100 --showvulgar yes > walnut_tx.$SGE_TASK_ID.gff
+
+#end=$(date +%s.%N)
+#runtime=$(python -c "print(${end}-${start})")
+
+#echo "Runtime for testshortproteome.vvi.$SGE_TASK_ID.gff was $runtime"
