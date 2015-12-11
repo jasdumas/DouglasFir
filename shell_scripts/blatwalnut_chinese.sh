2f7d0823785ff0784c86d35e62d6edaf2c205a01
Jasmine Dumas <jasdumas@users.noreply.github.com>
2015-12-11 14:04:15 -0500

    Update and rename blatwalnut.sh to blatwalnut_chinese.sh
    Correct and actual script that was submitted. Note changes to echo statements and database filepath and inclusion of $SGE_TASK_ID.fa

--- shell_scripts/blatwalnut_chinese.sh
+++ shell_scripts/blatwalnut_chinese.sh
@@ -5,20 +5,22 @@
 ##### /common/template_smp.sh ###############################
 #############################################################
 
+#
+
 # Specify name to be used to identify this run
-#$ -N blatwalnut
+#$ -N bw-chinese
 
-######$ -q highpri.q
+#$ -t 1-20:1                                                                                                                                             
 
-#$ -t 1-983:1                                                                                                                                                            
 # This sets the maximum number of concurrent tasks to 10, so that no more than 2 jobs will be run at once                                                               
-#$ -tc 4
+#$ -tc 5
 
-# Email address (change to yours)
+# email address
 #$ -M jasmine.dumas@gmail.com
 
-# Specify mailing options: b=beginning, e=end, s=suspended, n=never, a=aborted or suspended
+# mailing options:
 #$ -m e
+
 
 # Specify that bash shell should be used to process this script
 
@@ -28,18 +30,19 @@
 
 # Specify the output file
 
-#$ -o /archive/Walnut_genomes/blatwalnut_$JOB_ID.out
+#$ -o blatwalnut_ch_$JOB_ID.out
 
 # Specify the error file
 
-#$ -e /archive/Walnut_genomes/blatwalnut_$JOB_ID.err
+#$ -e blatwalnut_ch_$JOB_ID.err
 
 #$ -cwd
 # Run the program
-echo ChineseWalnut.500.scaffolds.fasta
+echo ChineseWalnut.500.scaffolds.fasta$SGE_TASK_ID.fa
 
 # change working directory
-cd /archive/Walnut_genomes/
+#cd /archive/Walnut_genomes/
 
 ## blat format: blat database query [-ooc=11.ooc] output.psl
-blat /archive/Walnut_genomes/ChineseWalnut.500.scaffolds.fasta  /archive/Walnut_genomes/spcombo.tfa -q=prot -t=dnax -maxIntron=800000 -fastMap -minScore=70 -minIdentity=60 spcombo$SGE_TASK_ID.psl
+blat /archive/Walnut_genomes/ChineseWalnut/ChineseWalnut.500.scaffolds.fasta$SGE_TASK_ID.fa /archive/Walnut_genomes/spcombo.tfa -q=prot -t=dnax -maxIntron=800000 -fastMap -minScore=70 -minIdentity=60 spcombo_ch_$SGE_TASK_ID.psl
+
